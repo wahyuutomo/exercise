eaf7e8b2e7f5a02be8cab307b9e4eae04b758722
wahyuutomo <wahyuutomo@users.noreply.github.com>
2015-09-21 04:55:14 +0700

    Update 003-doubledutch input 8 chars
    - input 8 chars
    - print string & chars

--- 003-doubledutch.c
+++ 003-doubledutch.c
@@ -6,9 +6,18 @@ void printString(char str[]);
 
 int main ()
 {
-	char myString[36] = "charcharchar";
+	char myString[36] = "charcharcharchar";
+	int i;
 	
 	printf("** Welcome to the Double Dutch game **\n");
+	
+	printf("Please enter a string of 8 chars: ");
+	
+	for (i=0;i<8;i++)
+	{
+		scanf("%1c",&myString[i]);
+	}
+	myString[i] = '\0';
 	
 	printString(myString);
 	
@@ -21,7 +30,7 @@ void printString(char str[])
 	int i;
 	printf("Printed as string: %s\n",str);
 	printf("printed as chars: ");
-	for (i=0;i<strlen(str);i++)
+	for (i=0;i<36;i++)
 	{
 		printf("%c",str[i]);
 	}
