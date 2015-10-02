8051f23999c61d17f66a5a5be402852fb8b79093
wahyuutomo <wahyuutomo@users.noreply.github.com>
2015-10-02 11:55:57 +0200

    Update 003-doubledutch replace chars
    - Add function replaceChars
    - Still has debug process

--- 003-doubledutch.c
+++ 003-doubledutch.c
@@ -5,15 +5,21 @@
 #include <stdlib.h>
 
 void printString(char str[]);
-void stringReorder(char str[], int index1, int index2);
+char * stringReorder(char str[], int index1, int index2);
 int findFirstOccurance(char str[], char aChar);
-
+char * replaceChars(char str[], char sChar[], char rChar);
 
 int main ()
 {
 	char myString[36] = "charcharcharchar";
 	char test[100];
+	char remove[] = "asd";
 	
+	printf("before = ");
+	printString(myString);
+	strcpy(myString,remove);
+	printf("after = ");
+	printString(myString);
 	
 	int i;
 	int idx=0;
@@ -25,7 +31,7 @@ int main ()
 	scanf(" %99[^\n]s", test);
 	
 	//printString(myString);
-	
+	printString(test);
 	
 	
 	printf("\n");
@@ -35,22 +41,29 @@ int main ()
 	idx=findFirstOccurance(test,'a');
 	printf("%d\n",idx);
 	
+	printf("----------------------\n");
+	printString(test);
+	printString(remove);
+	
+	printf("----------------------\n");
+	replaceChars(test,remove,'+');
+	printString(test);
 	return 0;
 }
 
 void printString(char str[])
 {
-	int i;
-	printf("Printed as string: %s\n",str);
-	printf("printed as chars: ");
-	for (i=0;i<36;i++)
-	{
-		printf("%c",str[i]);
-	}
+//	int i;
+	printf("%s\n",str);
+//	printf("printed as chars: ");
+//	for (i=0;i<36;i++)
+//	{
+//		printf("%c",str[i]);
+//	}
 	return;
 }
 
-void stringReorder(char str[], int index1, int index2)
+char * stringReorder(char str[], int index1, int index2)
 {
 	char part1[36]="\0";
 	char part2[36]="\0";
@@ -69,7 +82,9 @@ void stringReorder(char str[], int index1, int index2)
 	printf("Part 3: %s\n",part3);
 	printf("Result: %s\n",temp);
 	
-	return;
+	strcpy(str,temp);
+	
+	return str;
 }
  /*
 	void strclr(char string[])
@@ -84,21 +99,40 @@ void stringReorder(char str[], int index1, int index2)
 //*/
 int findFirstOccurance(char str[], char aChar)
 {
-	int val = 0;
-	char *cptr;
-	char *first;
-	
-	first = str;
-	
-	cptr = strchr(str, aChar);
-	if(cptr)
+	int j=0;
+	int stop = 0;
+	do 
 	{
-		val = cptr-first;
-		return val;
-	}
-	else 
+		if (str[j]==aChar) stop=1; 
+		j++;
+	} while ((stop==0)&&(str[j]!='\0'));
+	if (stop==1) {return j-1;} else {return -1;}
+	 
+}
+
+
+char * replaceChars(char str[], char sChar[], char rChar)
+{
+	int idx=0;
+	int j=0;
+	int i=0;
+	int lsChar=0;
+	
+	
+	
+	lsChar = strlen(sChar);
+	
+	for (j=0;j<lsChar;j++)
 	{
-		printf("character is not on the fucking array\n");
-		return -1;
+		
+		for (i=0;i<strlen(str);i++)
+		{
+			idx = findFirstOccurance(str,sChar[j]);
+			if (idx!=-1) str[idx] = rChar;
+		}
+		
+		
 	}
+	
+	return str;
 }
