b9a7e5351122df4fb1a46cc01e6ff83c8c396012
wahyuutomo <wahyuutomo@users.noreply.github.com>
2015-10-11 01:28:19 +0200

    Update 005-dictionary all function
    - complete all exercise function
    - not yet uploaded on peach

--- 005-dictionary.c
+++ 005-dictionary.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,64 +7,47 @@
 int numberOfWordsInDict(char **dict);
 void addWord(char **dict, char *word);
 void printDict(char **dict);
+void removeWord(char **dict, char *word);
+void swapWords(char **dict, char *word1, char *word2);
 
 int main ()
 {
 	
 	/**TESTING CORNER**/
-	/*
-	char *mem;
-	
-	char *string="teas";
-	mem = (char *) malloc(sizeof(char)*strlen(string));
-	//strcpy(mem,string);
-	//printf("String = %s,  Address = %u\n", mem, mem);
-	//*/
-
 	int i;
 	int nwords;
 	static char *dict[10];
 	char *test;
 	for (i=0;i<9;i++)
 		{dict[i]=NULL;}
-	/*
-	if (dict = (char **) malloc (10))
-	{
-		
-		for (i=0;i<9;i++)
-		{dict[i]=NULL;}
-	}
-	dict[0]="Stupid";
-	dict[1]="Baka";
-	char *string="teas";
-	//*/
-		
-	nwords = 0;
-
-	char* words;
+	
+	char* word;
+	char* word2;
 	
 	///--------------------------------------------///
-	
+	//*
+	dict[0]="anak";
+	dict[1]="kambing";
+	dict[2]="saya";
+	//*/
+	///--------------------------------------------///
+	//*
 	char op='\0';
 	while (1)
 	{
-		
-	//	printDict(dict);
-		printf("Command (a/p/q): ");
+		printf("Command (a/p/r/s/q): ");
 		if (scanf(" %c",&op)==1)
 		{
 			switch(op)
 			{
 				case ('a') : 
 				{
-					//free(test);
-					//test = malloc(20);
-					if (words = (char *) malloc(36))
+					if (word = (char *) malloc(36))
 					{
 						printf("Add a word: ");
-						scanf(" %[^\n]s", words);
-						addWord(dict,words);
-						free(words);
+						scanf(" %[^\n]s", word);
+						addWord(dict,word);
+						free(word);
 					}
 					
 					break;
@@ -73,11 +57,38 @@ int main ()
 					printDict(dict);
 					break;
 				}
+				case ('r') :
+				{
+					if (word = (char *) malloc(36))
+					{
+						printf("Remove a word: ");
+						scanf(" %[^\n]s", word);
+						removeWord(dict,word);
+						free(word);
+					}
+					break;
+				}
+				case ('s') :
+				{
+					if ((word = (char *) malloc(36))&&(word2 = (char *) malloc(36)))
+					{
+						printf("Swap two words:\n");
+						printf("Enter first word: ");
+						scanf(" %[^\n]s", word);
+						printf("Enter second word: ");
+						scanf(" %[^\n]s", word2);
+						swapWords(dict,word,word2);
+						free(word);
+						free(word2);
+					}
+					break;
+				}
 				case ('q') :  return 0;
 			}
 		}
 		
 	}
+	//*/
 	///--------------------------------------------///
 	
 		
@@ -88,10 +99,13 @@ int main ()
 	
 	//addWord(dict,"Babi");
 	//addWord(dict,"Enak");
-	//dict[0]="Stupid";
-	//dict[1]="Baka";
+	//printDict(dict);
+	dict[0]="Stupid";
+	dict[1]="Baka";
+	dict[2]="Goublog";
+	//removeWord(dict,"Baka");
 	//addWord(dict,"Sate");
-	//dict[2]="Goublog";
+	
 	//dict[3]="Wancur";
 	//printf("%s\n",dict[0]);
 	//printf("%s\n",dict[1]);
@@ -165,7 +179,6 @@ void addWord(char **dict, char *word)
 void printDict(char **dict)
 {
 	
-	char *word;
 	int i=0;
 	printf("Dictionary:\n");
 	
@@ -178,7 +191,47 @@ void printDict(char **dict)
 	if(i==0) printf("The dictionary is empty\n");
 }
 
+void removeWord(char **dict, char *word)
+{
+	int i=0;
+	int idx=0;
+	while (dict[i] != NULL)
+	{
+		if (strcmp(dict[i],word)==0)
+		{
+			
+			idx=numberOfWordsInDict(dict);
+			dict[i]=dict[idx-1];
+			//printf("\n\nindex %d : %s\n\n",idx,dict[idx]);
+			//printf("\n\nindex %d : %s\n\n",i,dict[i]);
+			dict[idx-1]=NULL;
+		}
+		i++;	
+	}
+}
 
+void swapWords(char **dict, char *word1, char *word2)
+{	
+	char*tmp;
+	int found=0,found2=0;
+	int i=0;
+	int idx=999,idx2=999;
+	while ((dict[i] != NULL)&&((found!=1)||(found2!=1)))
+	{
+		if ((found!=1)&&(strcmp(dict[i],word1)==0)) {found=1; idx=i; i=0;}
+		if ((found2!=1)&&(strcmp(dict[i],word2))==0) {found2=1; idx2=i; i=0;}
+		i++;	
+		//printf("\n\nidx found1= %d, idx found2= %d\n\n",found,found2);
+	} 
+	//printf("%d",i);
+	printf("\n\nidx found1= %d, idx found2= %d\n\n",idx,idx2);
+	if ((found==0)||(found2==0)) {printf("Cannot swap words. At least one word missing in the dictionary.\n"); return;}
+	dict[idx]=NULL;
+	addWord(dict,word2);
+	dict[idx2]=NULL;
+	addWord(dict,word1);
+	
+}
 ///-------------------TEMPLATE COMMAND-----------------------///
 	/*
 	char op='\0';
