df1343f826cb836a7ade6a3c213f3ef92278d428
wahyuutomo <wahyuutomo@users.noreply.github.com>
2015-10-11 00:08:59 +0200

    Update 005-dictionary fixing bug of addWord
    - fix bug add word to dict
    - still too many debug leftover

--- 005-dictionary.c
+++ 005-dictionary.c
@@ -11,22 +11,76 @@ int main ()
 {
 	
 	/**TESTING CORNER**/
-	//*
-	char test[34]="Hello World";
-
+	/*
+	char *mem;
+	
+	char *string="teas";
+	mem = (char *) malloc(sizeof(char)*strlen(string));
+	//strcpy(mem,string);
+	//printf("String = %s,  Address = %u\n", mem, mem);
 	//*/
 
 	int i;
 	int nwords;
-	static char **dict;
-	
-	dict = (char **) malloc (10);
-	for (i=0;i<10;i++)
-	{dict[i]=NULL;}
-	
+	static char *dict[10];
+	char *test;
+	for (i=0;i<9;i++)
+		{dict[i]=NULL;}
+	/*
+	if (dict = (char **) malloc (10))
+	{
+		
+		for (i=0;i<9;i++)
+		{dict[i]=NULL;}
+	}
+	dict[0]="Stupid";
+	dict[1]="Baka";
+	char *string="teas";
+	//*/
+		
 	nwords = 0;
+
+	char* words;
 	
+	///--------------------------------------------///
 	
+	char op='\0';
+	while (1)
+	{
+		
+	//	printDict(dict);
+		printf("Command (a/p/q): ");
+		if (scanf(" %c",&op)==1)
+		{
+			switch(op)
+			{
+				case ('a') : 
+				{
+					//free(test);
+					//test = malloc(20);
+					if (words = (char *) malloc(36))
+					{
+						printf("Add a word: ");
+						scanf(" %[^\n]s", words);
+						addWord(dict,words);
+						free(words);
+					}
+					
+					break;
+				}
+				case ('p') :
+				{
+					printDict(dict);
+					break;
+				}
+				case ('q') :  return 0;
+			}
+		}
+		
+	}
+	///--------------------------------------------///
+	
+		
 	/**TESTING CORNER**/
 	//*
 	
@@ -40,7 +94,7 @@ int main ()
 	//dict[2]="Goublog";
 	//dict[3]="Wancur";
 	//printf("%s\n",dict[0]);
-	//printf("%s\n",dict[1]);github
+	//printf("%s\n",dict[1]);
 	
 	printDict(dict);
 	
@@ -76,10 +130,32 @@ int numberOfWordsInDict(char **dict)
 void addWord(char **dict, char *word)
 {
 	int idx;
+	char *mem;
+	int r=0;
+
+	
+	//free(mem);
+	mem = (char *) malloc(sizeof(char)*(strlen(word)+1));
+//	while(word[r]!='\0')
+//	{
+//		mem[r]=word[r];
+//		r++;
+//	}
+//		mem[r]='\0';
+	//mem = (char *) malloc(15);
 	idx = numberOfWordsInDict(dict);
-	if (idx!=9)
+	
+	if (idx!=10)
 	{
-		dict[idx]=word;
+	//	printf("Idex = %d\n",idx);
+		//printDict(dict);
+		dict[idx]=mem;
+		strcpy(dict[idx],word);
+
+		//printDict(dict);
+		return;
+		
+		//printf("Idex %d contains word= %s\n",idx, dict[idx]);
 	} else 
 	{
 		printf("Dictionary is already full!! The word could not be added.\n");
@@ -101,3 +177,23 @@ void printDict(char **dict)
 	
 	if(i==0) printf("The dictionary is empty\n");
 }
+
+
+///-------------------TEMPLATE COMMAND-----------------------///
+	/*
+	char op='\0';
+	while (1)
+	{
+		printf("Command (p/g/r/s/q): ");
+		if (scanf(" %c",&op)==1)
+		{
+			switch(op)
+			{
+				case () : break;
+				case ('q') : return 0;
+			}
+		}
+		
+	}
+	//*/
+///---------------------------------------------------------///
